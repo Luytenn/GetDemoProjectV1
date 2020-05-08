5465c49c8b0f475c78d902edd2be900d41f2b5bd
Voyager <Voyager@17.10.1.11>
2020-05-08 17:56:25 -0500

    test 3 added commit....

---
+++ src/mipaquete/Test3.java
@@ -0,0 +1,18 @@
+package mipaquete;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+		
+		
+		if(hungry == "yes" || hungry=="y" || hungry=="Y")
+			System.out.println("eat something....");
+		else {
+			System.out.println("Do work..........");
+		}
+	}
+
+}
